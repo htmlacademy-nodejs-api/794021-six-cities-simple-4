8b7782c373b69a8e7b4874ea6e35863ae0d376f8
rrramble <rustam.arkharov@gmail.com>
2023-05-12 15:25:56 +0600

    feat: add command '--version' to 'main.cli.js'

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,10 +1,12 @@
 import CLIApplication from './app/cli.js';
 import HelpCommand from './core/cli-command/help.command.js';
+import VersionCommand from './core/cli-command/version.command.js';
 
 const cliManager = new CLIApplication();
 
 cliManager.registerCommands([
   new HelpCommand,
+  new VersionCommand,
 ]);
 
 cliManager.processCommand(process.argv);
