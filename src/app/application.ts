d59ff514dca16a5a27f0a3f6f88a38917f409b58
rrramble <rustam.arkharov@gmail.com>
2023-05-17 11:02:04 +0600

    refactor: move config initialization to main.ts from Application class

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,17 +1,18 @@
-import ConfigurationService from '../core/configuration/configuration.service.js';
+import { ConfigurationInterface } from '../core/configuration/configuration.interface.js';
+import { ConfigurationOptions } from '../core/configuration/configuration.schema.js';
 import { LoggerInterface } from '../core/logger/logger.interface.js';
 
 export default class Application {
   constructor(
-    private readonly logger: LoggerInterface
+    private readonly logger: LoggerInterface,
+    private readonly configuration: ConfigurationInterface<ConfigurationOptions>,
   ) {
   }
 
 
   public async init() {
     this.logger.info('Application initialized.');
-    const configuration = new ConfigurationService(this.logger);
-    console.log(configuration.get('INCOMING_TCP_PORT'));
+    this.logger.info(`Incoming TCP port: ${this.configuration.get('INCOMING_TCP_PORT')}`);
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { Logger } from './core/logger/logger.js';
 import Application from './app/application.js';
+import ConfigurationService from './core/configuration/configuration.service.js';
 
 
 async function bootstrap() {
   const logger = new Logger();
+  const configuration = new ConfigurationService(logger);
 
-  const application = new Application(logger);
-
+  const application = new Application(logger, configuration);
   await application.init();
 }
 
