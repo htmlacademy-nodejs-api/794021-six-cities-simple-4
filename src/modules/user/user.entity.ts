7344ee202eed077496877d4eede71781d990d818
Rustam A <rustam.arkharov@gmail.com>
2023-05-24 20:09:52 +0600

    feat: add 'user.entity'

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,11 +1,18 @@
 import typegoose, { defaultClasses, getModelForClass } from '@typegoose/typegoose';
+import { createPasswordHash } from '../../core/helpers/index.js';
+import { UserName } from '../../consts/users.js';
 import { User } from '../../types/user.type.js';
 
-const { prop } = typegoose;
+const { prop, modelOptions } = typegoose;
 
 export interface UserEntity extends defaultClasses.Base {
 }
 
+@modelOptions({
+  schemaOptions: {
+    collection: 'users',
+  }
+})
 export class UserEntity extends defaultClasses.TimeStamps implements User {
   @prop({ required: false, default: '' })
   public avatarPath = '';
@@ -13,14 +20,39 @@ export class UserEntity extends defaultClasses.TimeStamps implements User {
   @prop({ unique: true, required: true })
   public email = '';
 
-  @prop({ required: true })
+  @prop({
+    default: '',
+    maxLength: UserName.maxLength,
+    minLength: UserName.minLength,
+    required: true,
+  })
   public name = '';
 
-  @prop({ required: true })
+  @prop({ default: false, required: true })
   public isPro = false;
 
-  @prop({ required: true })
+  @prop({ default: '', required: true })
   public password = '';
+
+
+  constructor(userData: User) {
+    super();
+
+    this.avatarPath = userData.avatarPath;
+    this.email = userData.email;
+    this.name = userData.name;
+  }
+
+
+  public setPassword(password: string, salt: string) {
+    this.password = createPasswordHash(password, salt);
+  }
+
+
+  public getPassword() {
+    return this.password;
+  }
+
 }
 
 export const UserModel = getModelForClass(UserEntity);
