477336d265d99e660760ab8e17afff6e3921f235
Rustam A <rustam.arkharov@gmail.com>
2023-05-24 14:28:21 +0600

    feat: add _connectWithRetry() method in MongoClientService class

--- src/core/database-client/mongo-client.service.ts
+++ src/core/database-client/mongo-client.service.ts
@@ -1,7 +1,9 @@
+import { setTimeout } from 'node:timers/promises';
 import { inject, injectable } from 'inversify';
 import mongoose, { Mongoose } from 'mongoose';
 import { DatabaseClientInterface } from './database-client.interface.js';
 import { LoggerInterface } from '../logger/logger.interface.js';
+import { Retry } from '../../consts/database.js';
 import { ApplicationComponent } from '../../types/application-component.enum.js';
 
 
@@ -18,8 +20,26 @@ export default class MongoClientService implements DatabaseClientInterface {
 
 
   private async _connect(uri:string): Promise<void> {
-    this.mongooseInstance = await mongoose.connect(uri);
+    this.mongooseInstance = await this._connectWithRetry(uri);
     this.isConnected = true;
+  }
+
+
+  private async _connectWithRetry(uri: string): Promise<Mongoose> {
+    let attemptTry = 0;
+
+    while (attemptTry < Retry.CountLimit) {
+      try {
+        return await mongoose.connect(uri);
+      } catch (error) {
+        attemptTry++;
+        this.logger.error(`Failed to connect to the database (MongoDB): ${error}. Attempt number: ${attemptTry}`);
+        await setTimeout(Retry.Timeout); // TODO seems timeout is not working (waiting time lasts way more than set in the constant)
+      }
+    }
+
+    this.logger.error(`Failed to connect to the database (MongoDB) after ${attemptTry} attempts.`);
+    throw new Error('Failed to connect to the database (MongoDB).');
   }
 
 
