931175493224f6e9506c0e52d768d8804a9e5e58
Rustam A <rustam.arkharov@gmail.com>
2023-05-25 14:10:54 +0600

    feat: add service 'CreateUserDto'

---
+++ src/modules/user/dto/create-user.dto.ts
@@ -0,0 +1,7 @@
+export default class CreateUserDto { // TODO should name be UserDtoCreator ?
+  public avatarPath!: string;
+  public email!: string;
+  public isPro!: boolean;
+  public name!: string;
+  public password!: string;
+}
