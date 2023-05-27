7fa8015548915ac30bea9a7f70130fc952569ab7
Rustam A <rustam.arkharov@gmail.com>
2023-05-27 18:29:30 +0600

    feat: add func isEmailValid()

--- src/core/helpers/common.ts
+++ src/core/helpers/common.ts
@@ -4,3 +4,9 @@ export function createPasswordHash(text: string, salt: string): string {
   const shaHasher = crypto.createHmac('sha256', salt);
   return shaHasher.update(text).digest('hex');
 }
+
+
+export function isEmailValid(email: string): boolean {
+  const emailRegex = /^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  return emailRegex.test(email);
+}
