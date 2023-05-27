aaa05b8edd73c43ad0ef60ef93d125b732ce9b7b
Rustam A <rustam.arkharov@gmail.com>
2023-05-27 18:45:16 +0600

    feat: add 'createOfferContainer()' to 'main.ts'

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import Application from './app/application.js';
 import { createApplicationContainer } from './app/application.container.js';
 import { ApplicationComponent } from './types/application-component.enum.js';
 import { createUserContainer } from './modules/user/user.container.js';
+import { createOfferContainer } from './modules/offer/offer.container.js';
 
 
 async function bootstrap() {
   const applicationContainer = Container.merge(
     createApplicationContainer(),
     createUserContainer(),
+    createOfferContainer(),
   );
 
   const application = applicationContainer.get<Application>(ApplicationComponent.Application);
