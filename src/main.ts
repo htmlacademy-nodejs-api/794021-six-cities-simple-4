eec8ec6d6166ac08263ca59ea8aef676d616c12d
Rustam A <rustam.arkharov@gmail.com>
2023-06-04 18:02:48 +0600

    fix: add comment container to main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,15 @@ import { createApplicationContainer } from './app/application.container.js';
 import { ApplicationComponent } from './types/application-component.enum.js';
 import { createUserContainer } from './modules/user/user.container.js';
 import { createOfferContainer } from './modules/offer/offer.container.js';
+import { createCommentContainer } from './modules/comment/comment.container.js';
 
 
 async function bootstrap() {
   const applicationContainer = Container.merge(
     createApplicationContainer(),
-    createUserContainer(),
+    createCommentContainer(),
     createOfferContainer(),
+    createUserContainer(),
   );
 
   const application = applicationContainer.get<Application>(ApplicationComponent.Application);
